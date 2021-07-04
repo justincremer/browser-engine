883cf33fa077be029e8672885c13c1be914775e1
justincremer <jacremer@live.com>
2021-07-04 13:58:08 -0700

    adds gfx reification layer

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,13 @@
+#[macro_use]
+extern crate gfx;
+extern crate gfx_text;
+extern crate gfx_window_glutin;
+extern crate glutin;
+
+pub mod command;
 pub mod css;
 pub mod dom;
 pub mod html_parser;
 pub mod layout;
+pub mod render;
 pub mod style;
